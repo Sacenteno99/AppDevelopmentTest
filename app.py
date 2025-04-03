511a3cb48da6447ba285113c7cab88b538f4919c
Heber Samuel Centeno Martinez <hcentenomartinez@tulane.edu>
2025-04-03 12:04:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,9 @@ import streamlit as st
 
 st.title('My streamlit app')
 st.write('Hello World!')
+
+if st.button('Load Data'):
+    file = st.file_uploader("Upload a CSV file", type="csv")
+    if file is not None:
+        df = pd.read_csv(file)
+        st.write(df)
